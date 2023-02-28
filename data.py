750d4cfe6b2349155af9eb882db77ad2bd1293a1
shalini-mitt <shalini.mittal20@st.niituniversity.in>
2023-03-01 00:02:21 +0530

    m

--- data.py
+++ data.py
@@ -4,5 +4,5 @@ y_pred=[57880, 487400, 0.105320]
 from sklearn.metrics import r2_score
 r2 = r2_score(y_test, y_pred)
 print('r2 score for the random forest classifier model:', r2)
-print("RR")
+print("I love RR")
 print("SM")
