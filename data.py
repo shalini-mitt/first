a952bac5fcd2e3681ac71746d3a5064ee095fe3a
shalini-mitt <shalini.mittal20@st.niituniversity.in>
2023-03-13 16:04:58 +0530

    Revert "update"
    This reverts commit ac83349d842e35b9e944c5cbd5ecb90a913408ad.

--- data.py
+++ data.py
@@ -4,5 +4,9 @@ y_pred=[57880, 487400, 0.105320]
 from sklearn.metrics import r2_score
 r2 = r2_score(y_test, y_pred)
 print('r2 score for the random forest classifier model:', r2)
+<<<<<<< HEAD
 print("I love RR a lot")
 print("SM")
+=======
+print("RR")
+>>>>>>> parent of ac83349 (update)
